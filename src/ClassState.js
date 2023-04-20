ae918c511543dad4e910c6c88bbc557a4b639c2c
iamsergio22 <101826187+iamsergio22@users.noreply.github.com>
2023-04-20 11:23:15 -0500

    state declarative

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,22 +1,56 @@
 import React from "react";
+import Loading from "./Loading";
+
+const SECURITY_CODE = 'Hola123'
 
 class ClassState extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            error: false
+            error: false,
+            loading: false,
+            value: '',
+            Ckeck: false
+        }
+    }
+
+    componentDidUpdate() {
+        if (this.state.loading) {
+            setTimeout(() => {
+                console.log("haciendo validacion")
+                if (this.state.loading) {
+                    setTimeout(() => {
+                        console.log("haciendo validacion")
+                        this.setState({ loading: false })
+                        if (this.state.value === SECURITY_CODE) {
+                            this.setState({ loading: false, error: false, Ckeck: true })
+                        } else {
+                            this.setState({ error: true })
+                        }
+                        console.log("terminando validacion")
+                    }, 3000)
+                }
+                console.log("terminando validacion")
+            }, 3000)
         }
     }
     render() {
+        const { error, loading, value, Ckeck } = this.state;
+
         return (
             <div>
                 <h2>Eliminar ClassState</h2>
                 <p>Por favor, escribe el codigo de seguridad</p>
-                {this.state.error ? <p>Hay un error</p> : <p></p>}
-
-                <input type="text" placeholder="codigo de seguridad" name="" id="" />
+                {(error && !loading) ? <p>Hay un error</p> : <p></p>}
+                {loading && (<Loading />)}
+                {Ckeck ? <h4>Correcto</h4> : <h4></h4>}
+                <input type="text" placeholder="codigo de seguridad"
+                    onChange={(e) => {
+                        this.setState({ value: e.target.value })
+                    }}
+                    name="" id="" />
                 <button onClick={() => {
-                    this.setState({error: !this.state.error})
+                    this.setState({ loading: true })
                 }}>Comprobar</button>
             </div>
         )
--- src/UseState.js
+++ src/UseState.js
@@ -1,36 +1,91 @@
 import React, { useState } from 'react'
 
+const SECURITY_CODE = 'Hola123'
+
 function UseState() {
     //states
+    const [value, setValue] = useState('')
     const [error, setError] = useState(false)
     const [loading, setLoading] = useState(false)
-    //------------------
+    const [ckeck, setCkeck] = useState(false)
+    const [deleted, setdeleted] = useState(false)
+    const [confirmed, setConfirmed] = useState(false)
+    //------------------------------------------------------------------------------
     React.useEffect(() => {
         console.log("empezando efecto")
         if (loading) {
+            setError(false)
             setTimeout(() => {
                 console.log("haciendo validacion")
                 setLoading(false)
+                if (value === SECURITY_CODE) {
+                    setCkeck(true)
+                    setConfirmed(true)
+                } else {
+                    setError(true)
+                }
                 console.log("terminando validacion")
-            })
+            }, 3000)
         }
         console.log("terminando efecto")
     }, [loading])
-    return (
-        <div>
-            <h2>Eliminar UseState</h2>
-            <p>Por favor, escribe el codigo de seguridad</p>
-            {error ? <p>Hay un error</p> : <p></p>}
-            {loading && (
-                <p>Cargando...</p>
-            )}
-            <input type="text" placeholder="codigo de seguridad" name="" id="" />
+    if (!deleted && !confirmed) {
+        return (
+            <div>
+                <h2>Eliminar UseState</h2>
+                <p>Por favor, escribe el codigo de seguridad</p>
+                {error ? <p>Hay un error</p> : <p></p>}
+                {loading && (
+                    <p>Cargando...</p>
+                )}
+                {
+                    ckeck ? <h4>Clave correcta</h4> : <h4></h4>
+                }
+
+                <input type="text" placeholder="codigo de seguridad"
+                    value={value}
+                    onChange={(e) => {
+                        setValue(e.target.value)
+                    }}
+                    name="" id="" />
+                <button
+                    onClick={() => {
+                        // setError(false)
+                        setLoading(true)
+                    }}>Comprobar</button>
+            </div>
+        )
+    } else if (confirmed && !deleted) {
+        return (
+            <>
+                <p>
+                    ¿Desea eliminar?
+                </p>
+                <button
+                    onClick={() => {
+                        setdeleted(true)
+                        setValue('')
+                    }}
+                >Si, eliminar</button>
+                <button
+                    onClick={() => {
+                        setConfirmed(false)
+                        setValue('')
+                    }}
+                >No</button>
+            </>
+        )
+    } else {
+        return (<>
+            <h2>Eliminado con exito</h2>
             <button
                 onClick={() => {
-                    setError(true)
-                }}>Comprobar</button>
-        </div>
-    )
+                    setConfirmed(false)
+                    setdeleted(false)
+                }}
+            >Regresar a useState</button>
+        </>)
+    }
 }
 
 export default UseState
